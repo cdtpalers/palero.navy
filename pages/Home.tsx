5e3c94e3ad3a052e952fcfd7da7fc19b6393265e
Martin Simon Palero <palerski@Martins-MacBook-Air.local>
2025-12-12 19:15:28 +0800

    Feat: Replace background image with new JPG

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import heroBg from '../assets/hero-bg.png';
+import heroBg from '../assets/hero-bg.jpg';
 
 const Home: React.FC = () => {
   return (
